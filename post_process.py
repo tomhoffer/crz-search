afeb6cbc4255bf2bbea4be05a2627e6e8cd500e2
Tomas Hoffer <tomas.hoffer@gmail.com>
2021-10-16 21:40:27 +0200

    Post process: Remove spaces

--- post_process.py
+++ post_process.py
@@ -2,6 +2,13 @@ import pandas as pd
 
 lines = pd.read_json('output.jl', lines=True)
 
+# Drop duplicate items (just in case)
 lines = lines.drop_duplicates(subset='url')
+
+# Remove trailing spaces
+lines['customer'] = lines['customer'].str.strip()
+lines['supplier'] = lines['supplier'].str.strip()
+lines['customer_address'] = lines['customer_address'].str.strip()
+lines['supplier_address'] = lines['supplier_address'].str.strip()
 
 lines.to_csv('output.csv')
