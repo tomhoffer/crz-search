25fad78c14a0be42687accd444d961758a3b57ea
Tomas Hoffer <tomas.hoffer@gmail.com>
2021-10-23 16:02:29 +0200

    Main: Add examples

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from indexer.helpers import json_encode
 from indexer.indexer import EntityGraph
 
 entity_graph = EntityGraph()
 entity_graph.build_graph()
-#results = entity_graph.find_all_paths_iter('DOXX – STRAVNÉ LÍSTKY SPOL. S R. O.', 'BRATIA SABOVCI, S.R.O.', range=3)
-results = entity_graph.find_all_paths_iter('Zvolen', 'BRATIA SABOVCI, S.R.O.', range=4)
-#results = entity_graph.find_all_paths_iter('Všeobecná zdravotná poisťovňa, a.s.', 'Slovenská pošta, a. s.', range=3)
-#results = entity_graph.find_all_paths_from_node('Záhradnícka 151, Bratislava 821 08', min_range=1, max_range=5)
-print("Found paths:")
-print(json_encode(results))
 
-# print("Details of the contracts:")
-# details = entity_graph.find_contracts_for_given_paths(results)
-# print(json.dumps(details, indent=4, ensure_ascii=False))
+# Example: Same city
+# results = entity_graph.find_all_paths_iter('V. D. S. SPOL. S R. O.', 'ENBRA SLOVAKIA S.R.O.', range=3)
+
+# Example: Enclosed contract
+# results = entity_graph.find_all_paths_iter('DOXX – STRAVNÉ LÍSTKY SPOL. S R. O.', 'BRATIA SABOVCI, S.R.O.', range=3)
+
+# Example: Same address
+#results = entity_graph.find_all_paths_iter('SLOVAK TELEKOM, A. S.', 'T MOBILE SLOVAK TELEKOM, A.S.', range=3)
+
+
+
+results = entity_graph.find_all_paths_iter('SLOVENSKÁ POĽNOHOSPODÁRSKA UNIVERZITA V NITRE', 'T MOBILE SLOVAK TELEKOM, A.S.', range=4)
+
+
+
+# print(json_encode(results))
